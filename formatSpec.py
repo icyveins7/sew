60f789ebaf8539097c86284681efa18188067d9d
unknown <icyveins@hotmail.com>
2022-12-19 18:43:31 +0800

    add format specification class for easy construction

---
+++ formatSpec.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 16:41:17 2022
+
+@author: seo
+"""
+
+class FormatSpecifier:
+    sqliteTypes = {
+        int: "INTEGER",
+        float: "REAL",
+        str: "TEXT"
+    }
+    
+    def __init__(self, *args, **kwargs):
+        self.fmt = {'cols': [], 'conds': []}
+        
+    def clear(self):
+        self.fmt = {'cols': [], 'conds': []}
+        
+    def _getColumnNames(self):
+        return [i[0] for i in self.fmt['cols']]
+        
+    def addColumn(self, columnName: str, typehint: type):
+        self.fmt['cols'].append([columnName, self.sqliteTypes[typehint]])
+        
+    def addUniques(self, uniqueColumns: list):
+        if not all((i in self._getColumnNames() for i in uniqueColumns)):
+            raise ValueError("Invalid column found.")
+        self.fmt['conds'].append("UNIQUE (%s)" % (','.join(uniqueColumns)))
+        
+    def generate(self):
+        return self.fmt
+
+#%%
+if __name__ == "__main__":
+    fmtspec = FormatSpecifier()
+    fmtspec.addColumn('col1', int)
+    fmtspec.addColumn('col2', str)
+    
+    try:
+        fmtspec.addUniques(['col2', 'col3'])
+    except Exception as e:
+        print(e)
+        print("Should raise exception for invalid column.")
+        
+    fmtspec.addUniques(['col1', 'col2'])
+    print(fmtspec.generate())
