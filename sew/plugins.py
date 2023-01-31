387cd69eb47af052c340f1ffe88608037690dfeb
seoxubuntu <icyveins@hotmail.com>
2023-01-31 19:36:10 +0800

    update the numpy table parser with column logic and memory allocs (incomplete)

--- sew/plugins.py
+++ sew/plugins.py
@@ -110,7 +110,9 @@ class NumpyCommonMethodMixin(CommonRedirectMixin):
 
         2) On selects, convert column into numpy arrays of the associated data type if specified.
         This means that the default behaviour is to return as columns as opposed to rows.
+
         3) On inserts, check for data type if column has suffix specified.
+        
         4) Provide automatic dereferencing of array->column if inserts are done with multiple arrays.
     '''
 
@@ -136,14 +138,86 @@ class NumpyCommonMethodMixin(CommonRedirectMixin):
         return results
 
 class NumpyTableProxy(TableProxy):
+    numpyColumnSuffixes = {
+        "u64": np.uint64,
+        "u32": np.uint32,
+        "u16": np.uint16,
+        "u8": np.uint8,
+        "i64": np.int64,
+        "i32": np.int32,
+        "i16": np.int16,
+        "i8": np.int8,
+        "f64": np.float64,
+        "f32": np.float32,
+        "f16": np.float16
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._npresults = None # Additional cache for numpy results
         
-    @property
-    def npresults(self):
-        '''Getter for numpy dataframe results.'''
-        return self._npresults
+    def _getNumpyTypeFromSuffix(self, key: str):
+        pass # TODO
+
+    def fetchAsNumpy(self, firstLength: int=100):
+        '''
+        Calls fetch and parses the data into numpy arrays for each column if suffixes are defined;
+        if no appropriate suffix is found, it is returned as a row.
+
+        Parameters
+        ----------
+        firstLength : int
+            The first length of the arrays to preallocate.
+            Due to the nature of sqlite, the number of rows returned is not known until 
+            the fetch is complete. Hence, to minimise re-allocations, try to assign a
+            number larger than the expected number of rows to this.
+        '''
+
+        # Require use of sqlite.Row
+        if self._parent.con.row_factory is not sq.Row:
+            raise ValueError("Cannot use fetchAsNumpy() unless sqlite3.Row is set as row_factory.")
+
+        # We will return a dictionary of column names
+        r = dict()
+        # Iterate while fetching
+        inited = False
+        i = 0 # Our counter
+        while True:
+            # Fetch a row
+            row = self._parent.cur.fetchone()
+            if row is None:
+                break # No more rows to fetch
+            
+            if not inited:
+                for key in row.keys():
+                    numpytype = self._getNumpyTypeFromSuffix(key)
+                    if numpytype is not None:
+                        # Instantiate a numpy array of appropriate type
+                        r[key] = np.zeros(firstLength, dtype=numpytype)
+                    else:
+                        # Otherwise just make a list
+                        r[key] = list()
+
+                # Make sure we don't keep initing
+                inited = True
+
+            # Now iterate over the columns and write to respective arrays/lists
+            for key in row.keys():
+                # Check that our array has room left
+                if i >= r[key].size:
+                    # Otherwise replace with double the size
+                    tmp = np.zeros(r[key].size * 2, dtype=r[key].dtype)
+                    tmp[:r[key].size] = r[key][:]
+                    r[key] = tmp
+                
+                # Write to our array
+                r[key][i] = row[key]
+
+            # Increment counter
+            i += 1
+
+        return r
+
     
     def select(self,
                columnNames: list,
